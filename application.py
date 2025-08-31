8ca16b8217f829d4db643f1510894276f14e4c39
Harshit dubey <harshitdubey7896@gmail.comgit config --global user.email harshitdubey7896@gmail.com>
2025-08-31 13:00:47 +0530

    Updated main route to return index.html

--- application.py
+++ application.py
@@ -11,7 +11,7 @@ Standard_Scaler = pickle.load(open("models/scaler.pkl", "rb"))
 
 @app.route("/")
 def index():
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route("/predictdata",methods=["GET","POST"])
 def predict_datapoints():
